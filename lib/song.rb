5eb7c51fca4648b9add9dc34e1f0ce24ea3d37f5
Michael <mtrix92@gmail.com>
2022-06-23 10:10:22 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,17 @@
+class Song 
+    
+    attr_accessor :name, :artist, :genre
+    @@all = []
+    
+    def initialize(name, artist = nil, genre = nil)
+        @name = name
+        @artist = artist
+        @genre = genre 
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+end
